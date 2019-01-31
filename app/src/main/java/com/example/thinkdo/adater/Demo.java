e81de0b14c95b44ff23ec1b15bf8cce847326bbf
xiehua-1210 <1210618240@qq.com>
2019-02-01 00:20:06 +0800

    add Demo
    add Demo

---
+++ app/src/main/java/com/example/thinkdo/adater/Demo.java
@@ -0,0 +1,10 @@
+package com.example.thinkdo.adater;
+
+public class Demo {
+
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+
+    public static native void syaHello();
+}
